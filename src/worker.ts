a7ab77821db1eb0d7bd7920503c53d0cf9971733
Dominik Szopa <dszopa@gmail.com>
2019-07-24 12:52:08 +0200

    Changes to move js file to separate dir

--- src/worker.ts
+++ src/worker.ts
@@ -1,10 +1,17 @@
-import { getTinlakeData } from 'src/services/tinlake';
+import { getTinlakeData } from 'services/tinlake';
 import Datastore from 'nedb';
 import EventAPI from 'services/graphqlsettings/datasources/event';
 
 const db = new Datastore({ filename: '.tinlake_events.db', autoload: true });
 
-const api = new EventAPI({ store: db });
+const dataSources = () => ({
+  eventAPI: new EventAPI({ store: db }),
+});
 
-//const data = getTinlakeData();
-//data.then(result => api.createEvent(result));
+var api = dataSources.eventAPI();
+//const api = new EventAPI({ store: db });
+
+
+
+const data = getTinlakeData();
+data.then(result => api.createEvent(result));
