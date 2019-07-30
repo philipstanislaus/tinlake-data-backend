8b48502209a680a92c2ac45431948db3689f2d92
Philip Stanislaus <6912756+philipstanislaus@users.noreply.github.com>
2019-07-30 14:03:20 +0200

    Fixes

--- src/services/tinlake/index.ts
+++ src/services/tinlake/index.ts
@@ -3,7 +3,7 @@ import contractAddresses from './addresses_tinlake.json';
 import { sign } from 'ethjs-signer';
 const SignerProvider = require('ethjs-provider-signer');
 import config from './../../config';
-import BigNumber from 'bignumber.js';
+import BN from 'BN.js';
 
 export async function getTinlake() {
   return new Tinlake(
@@ -19,76 +19,67 @@ export async function getTinlake() {
   );
 }
 
-async function getTotalDept(tinlake, allLoans) {
-  var debt = 0;
+async function getTotalDept(tinlake: Tinlake, allLoans) {
+  let debt = 0;
 
-  for (var loanID in allLoans)
-  {
-    var loan = allLoans[loanID];
-    var LoanIDBN = new BigNumber(loanID.toString());
-    var BalanceDebt = await tinlake.getBalanceDebt(LoanIDBN);
-    console.log('BalanceDebt '+BalanceDebt.debt);
+  for (const loanID in allLoans) {
+    const loan = allLoans[loanID];
+    const LoanIDBN = loanID.toString();
+    const BalanceDebt = await tinlake.getBalanceDebt(LoanIDBN);
+    console.log(`BalanceDebt ${BalanceDebt.debt}`);
     if (BalanceDebt.debt > 0) {
-      console.log('Loan ID is '+ LoanIDBN);
+      console.log(`Loan ID is ${LoanIDBN}`);
 
-
-      var cur_dept = await tinlake.getCurrentDebt(LoanIDBN);
-      console.log('dept is '+cur_dept);
+      const cur_dept = await tinlake.getCurrentDebt(LoanIDBN);
+      console.log(`CurrentDebt is ${cur_dept}`);
       debt += cur_dept;
     }
 
   }
-
 
   return debt;
 
 }
 
 async function getAllLoans(tinlake, loansCount) {
-  let loans = [];
-  for (var loanId = 1; loanId < loansCount; loanId++) {
-    let loan = await tinlake.getLoan(loanId);
+  const loans = [];
+  for (let loanId = 0; loanId < loansCount; loanId += 1) {
+    const loan = await tinlake.getLoan(loanId);
 
     loans[loanId] = loan;
   }
-  return loans
+  return loans;
 }
 
 export async function getTinlakeData() {
   const tinlake = await getTinlake();
 
-  var NumberOfLoans = await tinlake.contracts.title.count();
-  NumberOfLoans = Number(NumberOfLoans[0].toString());
+  const loanCountBn: BN = await tinlake.loanCount();
 
-
-
-  const allLoans = await getAllLoans(tinlake, NumberOfLoans);
-
-
+  const allLoans = await getAllLoans(tinlake, loanCountBn.toNumber());
 
   try {
-        const TotalDebt = await getTotalDept(tinlake, allLoans);
-        console.log('TotalDebt is '+TotalDebt);
-    }
-    catch(err) {
-        console.log('Error: ', err.message);
-    }
+    const TotalDebt = await getTotalDept(tinlake, allLoans);
+    console.log(`TotalDebt is ${TotalDebt}`);
+  } catch (err) {
+    console.log(`Error: ${err.message}`);
+  }
 
-  //console.log(TotalDebt);
-  //const TotalBalance = await tinlake.contracts.pile.Balance();
-  //const TotalValueofNFTs = await tinlake.contracts.shelf.bags();
+  // console.log(TotalDebt);
+  // const TotalBalance = await tinlake.contracts.pile.Balance();
+  // const TotalValueofNFTs = await tinlake.contracts.shelf.bags();
   //
-  //const TotalSupply = await tinlake.contracts.collateral.totalSupply();
+  // const TotalSupply = await tinlake.contracts.collateral.totalSupply();
   //
   //
   //
-  //const data = {
+  // const data = {
   //  total_debt: Number(TotalDebt[0].toString()),
   //  total_balance: Number(TotalBalance[0].toString()),
   //  total_value_of_nfts: Number(TotalValueofNFTs[0].toString()),
   //  total_supply: Number(TotalSupply[0].toString()),
   //  number_of_loans: Number(NumberOfLoans[0].toString()),
-  //};
+  // };
   //
-  //return data;
+  // return data;
 }
--- src/worker.ts
+++ src/worker.ts
@@ -1,17 +1,18 @@
 import { getTinlakeData } from './services/tinlake';
-import { eventAPI } from './config'
-import config from './config'
-
+import { eventAPI } from './config';
+import config from './config';
 
 const data = getTinlakeData();
-    //data.then(result => eventAPI.createEvent(result));
-    //console.log(new Date()+'New event saved to db');
 
-//var CronJob = require('cron').CronJob;
+tinlake.getCurrentDebt(LoanIDBN);
+    // data.then(result => eventAPI.createEvent(result));
+    // console.log(new Date()+'New event saved to db');
+
+// var CronJob = require('cron').CronJob;
 //
-//new CronJob('*/'+config['runEveryMinute']+' * * * *', function() {
+// new CronJob('*/'+config['runEveryMinute']+' * * * *', function() {
 //    const data = getTinlakeData();
 //    data.then(result => eventAPI.createEvent(result));
 //    console.log(new Date()+'New event saved to db');
 //
-//}, null, true, 'UTC');
+// }, null, true, 'UTC');
