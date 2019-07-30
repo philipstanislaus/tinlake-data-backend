b6b227f5549d60e28e625f5718d03c0f8bffb2d3
Dominik Szopa <dszopa@gmail.com>
2019-07-30 10:41:49 +0200

    dubug code

--- src/services/tinlake/index.ts
+++ src/services/tinlake/index.ts
@@ -3,6 +3,7 @@ import contractAddresses from './addresses_tinlake.json';
 import { sign } from 'ethjs-signer';
 const SignerProvider = require('ethjs-provider-signer');
 import config from './../../config';
+import BigNumber from 'bignumber.js';
 
 export async function getTinlake() {
   return new Tinlake(
@@ -18,21 +19,76 @@ export async function getTinlake() {
   );
 }
 
+async function getTotalDept(tinlake, allLoans) {
+  var debt = 0;
+
+  for (var loanID in allLoans)
+  {
+    var loan = allLoans[loanID];
+    var LoanIDBN = new BigNumber(loanID.toString());
+    var BalanceDebt = await tinlake.getBalanceDebt(LoanIDBN);
+    console.log('BalanceDebt '+BalanceDebt.debt);
+    if (BalanceDebt.debt > 0) {
+      console.log('Loan ID is '+ LoanIDBN);
+
+
+      var cur_dept = await tinlake.getCurrentDebt(LoanIDBN);
+      console.log('dept is '+cur_dept);
+      debt += cur_dept;
+    }
+
+  }
+
+
+  return debt;
+
+}
+
+async function getAllLoans(tinlake, loansCount) {
+  let loans = [];
+  for (var loanId = 1; loanId < loansCount; loanId++) {
+    let loan = await tinlake.getLoan(loanId);
+
+    loans[loanId] = loan;
+  }
+  return loans
+}
+
 export async function getTinlakeData() {
   const tinlake = await getTinlake();
-  const TotalDebt = await tinlake.contracts.pile.Debt();
-  const TotalBalance = await tinlake.contracts.pile.Balance();
-  const TotalValueofNFTs = await tinlake.contracts.shelf.bags();
-  const NumberOfLoans = await tinlake.contracts.title.count();
-  const TotalSupply = await tinlake.contracts.collateral.totalSupply();
 
-  const data = {
-    total_debt: Number(TotalDebt[0].toString()),
-    total_balance: Number(TotalBalance[0].toString()),
-    total_value_of_nfts: Number(TotalValueofNFTs[0].toString()),
-    total_supply: 0,
-    number_of_loans: Number(NumberOfLoans[0].toString()),
-  };
+  var NumberOfLoans = await tinlake.contracts.title.count();
+  NumberOfLoans = Number(NumberOfLoans[0].toString());
 
-  return data;
+
+
+  const allLoans = await getAllLoans(tinlake, NumberOfLoans);
+
+
+
+  try {
+        const TotalDebt = await getTotalDept(tinlake, allLoans);
+        console.log('TotalDebt is '+TotalDebt);
+    }
+    catch(err) {
+        console.log('Error: ', err.message);
+    }
+
+  //console.log(TotalDebt);
+  //const TotalBalance = await tinlake.contracts.pile.Balance();
+  //const TotalValueofNFTs = await tinlake.contracts.shelf.bags();
+  //
+  //const TotalSupply = await tinlake.contracts.collateral.totalSupply();
+  //
+  //
+  //
+  //const data = {
+  //  total_debt: Number(TotalDebt[0].toString()),
+  //  total_balance: Number(TotalBalance[0].toString()),
+  //  total_value_of_nfts: Number(TotalValueofNFTs[0].toString()),
+  //  total_supply: Number(TotalSupply[0].toString()),
+  //  number_of_loans: Number(NumberOfLoans[0].toString()),
+  //};
+  //
+  //return data;
 }
--- src/worker.ts
+++ src/worker.ts
@@ -2,11 +2,16 @@ import { getTinlakeData } from './services/tinlake';
 import { eventAPI } from './config'
 import config from './config'
 
-var CronJob = require('cron').CronJob;
 
-new CronJob('*/'+config['runEveryMinute']+' * * * *', function() {
-    const data = getTinlakeData();
-    data.then(result => eventAPI.createEvent(result));
-    console.log(new Date()+'New event saved to db');
+const data = getTinlakeData();
+    //data.then(result => eventAPI.createEvent(result));
+    //console.log(new Date()+'New event saved to db');
 
-}, null, true, 'UTC');
+//var CronJob = require('cron').CronJob;
+//
+//new CronJob('*/'+config['runEveryMinute']+' * * * *', function() {
+//    const data = getTinlakeData();
+//    data.then(result => eventAPI.createEvent(result));
+//    console.log(new Date()+'New event saved to db');
+//
+//}, null, true, 'UTC');
